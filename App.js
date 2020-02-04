e3cb6b07c5f840b1ab41d1be8b72a38de7ce5161
DS Hwang <hds2g22@gmail.com>
2020-02-04 18:00:29 +0900

    Asking for Permissions

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import React from "react";
+import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
 
 export default class extends React.Component {
-  getLocation = async () => {
-    const location = await Location.getCurrentPositionAsync();
-    console.log(location);
+  state = {
+    isLoading: true
   };
+  getLocation = async () => {
+    try {
+      await Location.requestPermissionsAsync();
+      const {
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
+      this.setState({ isLoading: false });
+      console.log(latitude, longitude);
+    } catch (error) {
+      Alert.alert("Can't fund you.");
+      console.log(error);
+    }
+  };
+
   componentDidMount() {
     this.getLocation();
   }
